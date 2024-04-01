1e0cd36aadf0f316e97c0ceb18355ab23a83196a
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-01 16:20:02 +0700

    Masukkan Fungsi Nama & Nomor mhs dengan Getline

---
+++ Sturct3Pemdas/Sturct3Pemdas.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+}
