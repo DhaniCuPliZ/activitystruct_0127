2203d9542d6d5516faf3206588d9ee8cd9a73a02
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-01 17:07:18 +0700

    Input data mhs menggunakan For

--- Struct4Pemdas/Struct4Pemdas.cpp
+++ Struct4Pemdas/Struct4Pemdas.cpp
@@ -17,4 +17,16 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; < 3; i++)
-		
+		cout << "Nomor Mahasiswa : ";
+		getline(cin, mhs.nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs.nama);
+
+		cout << "Alamat Mahasiswa : " << endl;
+		cout << "\t Nama Desa : ";
+		getline(cin, mhs.alamat.desa);
+		cout << "\t Nama Kota : ";
+		getline(cin, mhs.alamat.kota);
+		cout << "Umur Mahasiswa : ";
+		cin >> mhs.umur;
+	}
