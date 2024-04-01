ff36583761ce08b24569b0ad4bbc8cfaf840e23a
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-01 17:02:36 +0700

    Deklrasikan Array mhs

---
+++ Struct4Pemdas/Struct4Pemdas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+	
+		
