ec669fdc45cc20e49c682e767c081a291fae9110
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-01 17:31:19 +0700

    masukkan alamat detail mhs buat for loop

--- Struct4Pemdas/Struct4Pemdas.cpp
+++ Struct4Pemdas/Struct4Pemdas.cpp
@@ -36,7 +36,10 @@ int main() {
 for (int i = 0; i < 3; i++) {
 	cout << endl;
 	cout << "Data ke-" << (i + 1) << ":" << endl;
-	cout << "\n NIM : " << mhs.nim;
-	cout << "\n Nama : " << mhs.nama;
+	cout << "\n NIM : " << mhs[i].nim;
+	cout << "\n Nama : " << mhs[i].nama;
+	cout << "\n Alamat : ";
+	cout << "\n \t Desa : " << mhs[i].alamat.desa;
+	cout << "\n \t Kota : " << mhs[i].alamat.kota;
 
 }
