396189776f2fa0f4ba5907fd5582737183697553
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-01 17:34:10 +0700

    masukkan umur buat for loop

--- Struct4Pemdas/Struct4Pemdas.cpp
+++ Struct4Pemdas/Struct4Pemdas.cpp
@@ -16,7 +16,7 @@ struct Mahasiswa {
 
 int main() {
 	Mahasiswa mhs[3];
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++) {
 		cout << "Data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor Mahasiswa : ";
 		getline(cin, mhs[i].nim);
@@ -33,13 +33,14 @@ int main() {
 		cin.ignore(1, '\n');
 		cout << endl;
 	}
-for (int i = 0; i < 3; i++) {
-	cout << endl;
-	cout << "Data ke-" << (i + 1) << ":" << endl;
-	cout << "\n NIM : " << mhs[i].nim;
-	cout << "\n Nama : " << mhs[i].nama;
-	cout << "\n Alamat : ";
-	cout << "\n \t Desa : " << mhs[i].alamat.desa;
-	cout << "\n \t Kota : " << mhs[i].alamat.kota;
-
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data ke-" << (i + 1) << ":" << endl;
+		cout << "\n NIM : " << mhs[i].nim;
+		cout << "\n Nama : " << mhs[i].nama;
+		cout << "\n Alamat : ";
+		cout << "\n \t Desa : " << mhs[i].alamat.desa;
+		cout << "\n \t Kota : " << mhs[i].alamat.kota;
+		cout << "\n Umur : " << mhs[i].umur;
+	}
 }
